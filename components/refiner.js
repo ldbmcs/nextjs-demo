8a1a72993e67a7a55a8f3089d21a9146369b547f
dengchaosheng <dengchaosheng@jinshuju.net>
2022-11-30 11:42:40 +0800

    Update fefiner

--- components/refiner.js
+++ components/refiner.js
@@ -1,1 +1,6 @@
 window._refinerQueue = window._refinerQueue || []; function _refiner(){_refinerQueue.push(arguments);} _refiner('setProject', '33208ad0-24e4-11ed-9313-1d7ec5d3ecff'); (function(){var a=document.createElement("script");a.type="text/javascript";a.async=!0;a.src="https://js.refiner.io/v001/client.js";var b=document.getElementsByTagName("script")[0];b.parentNode.insertBefore(a,b)})();
+_refiner('identifyUser', {
+  id: '32cf1380-24e4-11ed-a7c9-b931932a94c4',
+  email: 'ldbmcs@gmail.com',
+  name: 'Deng Ldbmcs',
+});
