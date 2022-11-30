a6735ec9043db8b9d06882a2fda9b4ebd3aea9e4
dengchaosheng <dengchaosheng@jinshuju.net>
2022-11-30 19:43:55 +0800

    Update howxm

--- components/howxm.js
+++ components/howxm.js
@@ -1,11 +1,11 @@
-  function _howxm(){_howxmQueue.push(arguments)}
-  window._howxmQueue=window._howxmQueue||[];
-  _howxm('setAppID','c6fe6b87-d301-42b6-be42-2792f7b4ac1c');
-  (function(){var scriptId='howxm_script';
-  if(!document.getElementById(scriptId)){
-  var e=document.createElement('script'),
-  t=document.getElementsByTagName('script')[0];
-  e.setAttribute('id',scriptId);
-  e.type='text/javascript';e.async=!0;
-  e.src='https://static.howxm.com/sdk.js';
-  t.parentNode.insertBefore(e,t)}})();
+    function _howxm(){_howxmQueue.push(arguments)}
+    window._howxmQueue=window._howxmQueue||[];
+    _howxm('setAppID','c84003ce-1f9c-4a57-98f8-906c8d34d142');
+    (function(){var scriptId='howxm_script';
+    if(!document.getElementById(scriptId)){
+    var e=document.createElement('script'),
+    t=document.getElementsByTagName('script')[0];
+    e.setAttribute('id',scriptId);
+    e.type='text/javascript';e.async=!0;
+    e.src='https://static-usd.howxm.com/sdk.js';
+    t.parentNode.insertBefore(e,t)}})();
