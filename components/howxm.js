425737c987878bebb624241c5609e2066fbb881e
dengchaosheng <ldbmcs@gmail.com>
2023-01-11 17:09:46 +0800

    Update howxm sdk env to prod

--- components/howxm.js
+++ components/howxm.js
@@ -1,14 +1,16 @@
-function _howxm(){_howxmQueue.push(arguments)}
-window._howxmQueue=window._howxmQueue||[];
-_howxm('setAppID','a27e26ef-dca0-425c-a061-ff0de77b7562');
-(function(){var scriptId='howxm_script';
+<!-- Start of Howxm client code snippet -->
+    function _howxm(){_howxmQueue.push(arguments)}
+    window._howxmQueue=window._howxmQueue||[];
+    _howxm('setAppID','c6fe6b87-d301-42b6-be42-2792f7b4ac1c');
+    (function(){var scriptId='howxm_script';
     if(!document.getElementById(scriptId)){
-        var e=document.createElement('script'),
-            t=document.getElementsByTagName('script')[0];
-        e.setAttribute('id',scriptId);
-        e.type='text/javascript';e.async=!0;
-        e.src='http://localhost:4000/umd/sdk.js';
-        t.parentNode.insertBefore(e,t)}})();
+    var e=document.createElement('script'),
+    t=document.getElementsByTagName('script')[0];
+    e.setAttribute('id',scriptId);
+    e.type='text/javascript';e.async=!0;
+    e.src='https://static.howxm.com/sdk.js';
+    t.parentNode.insertBefore(e,t)}})();
+<!-- End of Howxm client code snippet -->
 
 _howxm('identify', {
     uid: '11',
